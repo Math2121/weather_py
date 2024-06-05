a2b97a76c16831b71f8b3be288d5459bc5fd991d
Matheus da Silva de Paula <matheusdepaula@wisecut.video>
2024-06-05 14:43:46 -0300

    feat: init project

---
+++ weather.py
@@ -0,0 +1,16 @@
+from dotenv import load_dotenv
+from pprint import pprint
+import os
+import requests
+
+load_dotenv()
+
+def get_current_weather(city="Rio de Janeiro"):
+    api_key = os.getenv('API_KEY')
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=imperial'
+
+    response = requests.get(url)
+    return response.json()
+
+if __name__ == '__main__':
+    pprint(get_current_weather())
